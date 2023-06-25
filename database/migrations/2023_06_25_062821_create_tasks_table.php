70f50a53a144efbd45da44e23528b16bcd9238e4
Temur Komilov <temur@pop-os.localdomain>
2023-06-25 11:36:11 +0500

    migrations

--- database/migrations/2023_06_25_062821_create_tasks_table.php
+++ database/migrations/2023_06_25_062821_create_tasks_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->integer('role')->default(0);
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('project_id');
+            $table->integer('user_id');
+            $table->text('task');
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
