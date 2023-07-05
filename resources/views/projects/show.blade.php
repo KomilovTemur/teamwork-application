c9994a1d5b54abede58f5807c3073d71cd196abb
Temur Komilov <itteamdev74@gmail.com>
2023-07-05 23:01:08 +0500

    some icons and text changes

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -8,14 +8,14 @@ Basecamp
 <nav class="navbar px-5 sticky-top bg-body-tertiary navbar-expand-lg bg-body-tertiary">
 
   <div class="container-fluid">
-    <a class="navbar-brand" href="{{route('projects.show', $project->id)}}"><i class="fas fa-code"></i> Project</a>
+    <a class="navbar-brand" href="{{ route('projects.show', $project->id) }}"><i class="fas fa-code"></i> Project</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#"><i class="fas fa-book"></i> Issues</a>
+          <a class="nav-link active" aria-current="page" href="#"><i class="fas fa-bug"></i> Issues</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#"><i class="fas fa-users"></i> Users</a>
@@ -24,14 +24,16 @@ Basecamp
           <a class="nav-link" href="#"><i class="fas fa-play-circle"></i> Actions</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><i class="fas fa-gear"></i> Settings</a>
-
+          <a class="nav-link" href="{{ route('projects.edit', $project->id) }}"><i class="fas fa-gear"></i> Settings</a>
         </li>
         <li class="nav-item">
           <a class="nav-link disabled">Disabled</a>
         </li>
       </ul>
     </div>
+    <span class="navbar-text">
+      <a class="nav-link" href="{{ route('dashboard') }}"><i class="fas fa-home"></i> Dashboard</a>
+    </span>
   </div>
 </nav>
 <div class="container">
@@ -43,6 +45,7 @@ Basecamp
 </div>
 @endsection
 @section('script')
+<script src="/assets/js/bootstrap.min.js"></script>
 <script src="https://cdn.ckeditor.com/ckeditor5/38.1.0/classic/ckeditor.js"></script>
 <script>
   // ClassicEditor
