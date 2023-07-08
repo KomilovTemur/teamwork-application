b5f34de1e61f9e145d7a7004857e165dcf5b1b86
Temur Komilov <itteamdev74@gmail.com>
2023-07-08 20:13:11 +0500

    attachments table  migration

--- database/migrations/2023_07_08_150917_create_attachment_project_table.php
+++ database/migrations/2023_07_08_150917_create_attachment_project_table.php
@@ -4,17 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('attachments', function (Blueprint $table) {
+        Schema::create('attachment_project', function (Blueprint $table) {
             $table->id();
-            $table->string('user_id');
-            $table->string('file');
-            $table->string('extension');
+            $table->integer('attachment_id');
+            $table->integer('project_id');
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('attachments');
+        Schema::dropIfExists('attachment_project');
     }
 };
