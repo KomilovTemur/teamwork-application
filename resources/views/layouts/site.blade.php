660371954fdd55d2ea949728d3df977884350601
Temur Komilov <itteamdev74@gmail.com>
2023-07-09 16:46:04 +0500

    some changes on blade files

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,18 +1,18 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Basecamp</title>
-    <link rel="stylesheet" href="/assets/css/main.css" />
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Basecamp</title>
+  <link rel="stylesheet" href="/assets/css/main.css" />
 </head>
 
 <body @yield('style')>
+  @yield('css')
+  @yield('content')
 
-    @yield('content')
-
-    @yield('script')
+  @yield('script')
 
 </body>
 
